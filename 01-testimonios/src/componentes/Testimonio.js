9b6be593b009a067041f353920e0506e5ab3590c
Jonatandb <jonatandb@gmail.com>
2022-03-17 15:00:44 -0300

    Styles added

--- 01-testimonios/src/componentes/Testimonio.js
+++ 01-testimonios/src/componentes/Testimonio.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../hojas-de-estilo/Testimonio.css'
 
 function Testimonio() {
   return (
@@ -15,7 +16,7 @@ function Testimonio() {
           Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el
           curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de
           datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba
-          para conseguir el trabajo de mis sueños como ingeniero de software en Spotify./>
+          para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.
         </p>
       </div>
     </div>
