534a00016f5f1282227cece561d7447a3cc5b0ef
Jonatandb <jonatandb@gmail.com>
2022-03-18 12:31:24 -0300

    Styling of App, logo and calculator wrapper

--- 03-calculadora/src/App.js
+++ 03-calculadora/src/App.js
@@ -1,7 +1,14 @@
 import './App.css'
-
+import freeCodeCampLogo from './images/freecodecamp-logo.png'
 function App() {
-  return <div className='App'>Calculadora</div>
+  return (
+    <div className='App'>
+      <div className='freeCodeCamp-logo-contenedor'>
+        <img src={freeCodeCampLogo} className='freeCodeCamp-logo' alt='Logo de freeCodeCamp' />
+      </div>
+      <div className='contenedor-calculadora'></div>
+    </div>
+  )
 }
 
 export default App
