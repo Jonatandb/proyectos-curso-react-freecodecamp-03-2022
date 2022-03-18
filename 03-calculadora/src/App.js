80b909bf1fd76694f631a83746d753d2f72bbc34
Jonatandb <jonatandb@gmail.com>
2022-03-18 12:45:22 -0300

    Button component added

--- 03-calculadora/src/App.js
+++ 03-calculadora/src/App.js
@@ -1,4 +1,5 @@
 import './App.css'
+import Boton from './components/Boton/Boton'
 import freeCodeCampLogo from './images/freecodecamp-logo.png'
 function App() {
   return (
@@ -6,7 +7,15 @@ function App() {
       <div className='freeCodeCamp-logo-contenedor'>
         <img src={freeCodeCampLogo} className='freeCodeCamp-logo' alt='Logo de freeCodeCamp' />
       </div>
-      <div className='contenedor-calculadora'></div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'>
+          <Boton>1</Boton>
+        </div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+      </div>
     </div>
   )
 }
---
+++ 03-calculadora/src/components/Boton/Boton.js
@@ -0,0 +1,3 @@
+export default function Boton(props) {
+  return <div>{props.children}</div>
+}
