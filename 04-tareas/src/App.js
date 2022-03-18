a53384dc82a22fe22bf6bd29571f671f232a4b57
Jonatandb <jonatandb@gmail.com>
2022-03-18 17:25:30 -0300

    Tarea component created

--- 04-tareas/src/App.js
+++ 04-tareas/src/App.js
@@ -1,10 +1,15 @@
 import './App.css'
 import LogoFreeCodeCamp from './components/LogoFreeCodeCamp/LogoFreeCodeCamp'
+import Tarea from './components/Tarea/Tarea'
 
 function App() {
   return (
     <div className='aplicacion-tareas'>
       <LogoFreeCodeCamp />
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <Tarea texto='Aprender React' />
+      </div>
     </div>
   )
 }
---
+++ 04-tareas/src/components/Tarea/Tarea.js
@@ -0,0 +1,12 @@
+import './Tarea.css'
+
+const Tarea = ({ texto }) => {
+  return (
+    <div className='tarea-contenedor'>
+      <div className='tarea-texto'>{texto}</div>
+      <div className='tarea-icono'>Eliminar</div>
+    </div>
+  )
+}
+
+export default Tarea
