2f5553fb4bf502ae0a03b2a31568b911c646d158
Jonatandb <jonatandb@gmail.com>
2022-03-18 19:52:51 -0300

    Improve to ListaDeTareas component to add and show the tasks list correctly

--- 04-tareas/src/components/ListaDeTareas/ListaDeTareas.js
+++ 04-tareas/src/components/ListaDeTareas/ListaDeTareas.js
@@ -1,14 +1,26 @@
 import Formulario from '../Formulario/Formulario'
 import Tarea from '../Tarea/Tarea'
 import './ListaDeTareas.css'
+import { useState } from 'react'
 
 export const ListaDeTareas = () => {
+  const [tareas, setTareas] = useState([])
+
+  const agregarTarea = tarea => {
+    if (tarea.texto.trim()) {
+      tarea.texto = tarea.texto.trim()
+      const tareasActualizadas = [tarea, ...tareas]
+      setTareas(tareasActualizadas)
+    }
+  }
+
   return (
     <>
-      <Formulario />
+      <Formulario onSubmit={agregarTarea} />
       <div className='tareas-lista-contenedor'>
-        <Tarea completada texto='Aprender Javascript' />
-        <Tarea texto='Aprender React' />
+        {tareas.map(tarea => (
+          <Tarea key={tarea.id} id={tarea.id} texto={tarea.texto} completada={tarea.completada} />
+        ))}
       </div>
     </>
   )
