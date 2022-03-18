76f8ff85cd930d2157e8162753bf97b3a6f0031c
Jonatandb <jonatandb@gmail.com>
2022-03-18 19:52:10 -0300

    Improve of Formulario component to allow to creat a new task on submit

--- 04-tareas/src/components/Formulario/Formulario.js
+++ 04-tareas/src/components/Formulario/Formulario.js
@@ -1,9 +1,33 @@
 import './Formulario.css'
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 const Formulario = props => {
+  const [input, setInput] = useState('')
+
+  const manejarCambio = e => {
+    setInput(e.target.value)
+  }
+
+  const manejarEnvio = e => {
+    e.preventDefault()
+    const tareaNueva = {
+      id: uuidv4(),
+      texto: input,
+      completada: false,
+    }
+    props.onSubmit(tareaNueva)
+  }
+
   return (
-    <form className='tarea-formulario'>
-      <input type='text' className='tarea-input' placeholder='Escribe una Tarea' name='texto' />
+    <form className='tarea-formulario' onSubmit={manejarEnvio}>
+      <input
+        type='text'
+        className='tarea-input'
+        placeholder='Escribe una Tarea'
+        name='texto'
+        onChange={manejarCambio}
+      />
       <button className='tarea-boton'>Agregar Tarea</button>
     </form>
   )
